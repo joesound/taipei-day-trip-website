5bab180111fda0716739b9c490373d65ab6607e6
wei <kuo0930414695@gmail.com>
2022-04-19 23:06:29 +0800

    week6 homework

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,14 @@
-TRADE_KEY = "partner_NzoWN3wZcZAsw44xLKNH8wEMsgw83p5DYPnNfqEsUoHftxNlx3mLwiCp"
-PARTNER_KEY = "partner_NzoWN3wZcZAsw44xLKNH8wEMsgw83p5DYPnNfqEsUoHftxNlx3mLwiCp"
 
-MYSQL_ACCOUNT = "root"
-MYSQL_PASSWORD = "Cwl@8506070618"
 
-URL_BS = "http://52.73.173.92:3000"
+def TRADE_KEY():
+    return "partner_NzoWN3wZcZAsw44xLKNH8wEMsgw83p5DYPnNfqEsUoHftxNlx3mLwiCp"
+def PARTNER_KEY():
+    return "partner_NzoWN3wZcZAsw44xLKNH8wEMsgw83p5DYPnNfqEsUoHftxNlx3mLwiCp"
+
+def MYSQL_ACCOUNT():
+    return "root"
+def MYSQL_PASSWORD():
+    return "Cwl@8506070618"
+
+def URL_BS():
+    return "http://52.73.173.92:3000"
